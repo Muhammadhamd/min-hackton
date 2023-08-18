5f661448aba9a5099f901fabe174384461f761dd
muhammadhamd <muhammadhamdali572@gmail.com>
2023-08-18 19:12:06 +0500

    fgaga

--- v1/routes/auth.mjs
+++ v1/routes/auth.mjs
@@ -59,12 +59,12 @@ router.post("/register", async (req, res) => {
       if (user) {
         // User with provided email or username found
         console.log("user already exists: ", user);
-        res.status(400).send({ message: "User already exists. Please try a different email or username." });
+        res.status(400).send( "User already exists. Please try a different email or username." );
         return;
       } else {
         const hashedPassword = await bcrypt.hash(newPassword, 10);
   
-        console.log("Hashed Password:", hashedPassword);
+        // console.log("Hashed Password:", hashedPassword);
   
         await Usermodel.create({
           username: newUserName,
@@ -75,6 +75,20 @@ router.post("/register", async (req, res) => {
   
         console.log(Usermodel);
         res.redirect("/")
+
+        const token = jwt.sign({
+          _id: data._id,
+          email: data.email,
+          iat: Date.now() / 1000 - 30,
+          exp: Date.now() / 1000 + (60 * 60 * 24),
+        }, SECRET);
+        console.log('token', token);
+  
+        res.cookie('token', token, {
+          maxAge: 86_400_000,
+          httpOnly: true,
+        });
+        
       }
     } catch (error) {
       console.error("Error during user registration:", error);
@@ -91,10 +105,10 @@ router.post("/login", async (req, res) => {
       { $or: [{ email: userAddress }, { username: userAddress }] },
       "username name email password"
     );
-
+0
     if (!data) {
       console.log("User not found");
-      return res.status(401).send({ message: "Incorrect email or password" });
+      return res.status(401).send( "Incorrect email or password" );
     }
 
     const isMatch = await bcrypt.compare(userPassword, data.password);
@@ -115,14 +129,7 @@ router.post("/login", async (req, res) => {
         httpOnly: true,
       });
 
-      res.send({
-        message: "Login successfully",
-        profile: {
-          email: data.email,
-          username: data.username,
-          name: data.name
-        }
-      });
+      res.redirect("/")
     } else {
       console.log("Password did not match");
       return res.status(401).send({ message: "Incorrect password" });
@@ -139,6 +146,7 @@ router.post("/logout", (req, res) => {
       maxAge: 1,
       httpOnly: true
   });
+  res.redirect('/registration')
 
   res.send({ message: "Logout successful" });
 })
